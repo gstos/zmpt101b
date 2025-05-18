61c9f0793b2d95be437fa533873a7a7d2c767596
Gustavo Santos <gustavo@next.ventures>
2025-05-18 00:21:20 -0300

    new sensor.py using async

--- esphome/components/zmpt101b/sensor.py
+++ esphome/components/zmpt101b/sensor.py
@@ -11,30 +11,33 @@ from esphome.const import (
     ICON_FLASH,
     )
 
-# Namespace for your component
 zmpt101b_ns = cg.esphome_ns.namespace("zmpt101b")
 ZMPT101BSensor = zmpt101b_ns.class_(
     "ZMPT101BSensor", sensor.Sensor, cg.PollingComponent
     )
 
-CONFIG_SCHEMA = sensor.sensor_schema(
-    unit_of_measurement=UNIT_VOLT,
-    icon=ICON_FLASH,
-    accuracy_decimals=1,
-    ).extend({
-    cv.GenerateID(): cv.declare_id(ZMPT101BSensor),
-    cv.Required(CONF_PIN): cv.int_,
-    cv.Optional(CONF_SENSITIVITY, default=1.0): cv.float_,
-    cv.Optional(CONF_FREQUENCY, default=50): cv.int_,
-    }).extend(cv.polling_component_schema("60s"))
-
-
-def to_code(config):
-    var = cg.new_Pvariable(
-        config[CONF_ID],
-        config[CONF_PIN],
-        config[CONF_SENSITIVITY],
-        config[CONF_FREQUENCY]
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(
+        unit_of_measurement=UNIT_VOLT,
+        icon=ICON_FLASH,
+        accuracy_decimals=1,
         )
-    yield cg.register_component(var, config)
-    yield sensor.register_sensor(var, config)
+    .extend({
+        cv.GenerateID(): cv.declare_id(ZMPT101BSensor),
+        cv.Required(CONF_PIN): cv.int_,
+        cv.Optional(CONF_SENSITIVITY, default=1.0): cv.float_,
+        cv.Optional(CONF_FREQUENCY, default=50): cv.int_,
+        })
+    .extend(cv.polling_component_schema("60s"))
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID], config[CONF_PIN])
+    await cg.register_component(var, config)
+    await sensor.register_sensor(var, config)
+
+    if sensitivity := config.get(CONF_SENSITIVITY):
+        cg.add(var.set_sensitivity(sensitivity))
+    if freq := config.get(CONF_FREQUENCY):
+        cg.add(var.set_frequency(freq))
